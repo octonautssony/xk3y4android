e2f485405b18279b007b248afb31157b52c996b1
gsourdrille <gsourdrille@gmail.com>
2013-05-04 14:09:02 +0000

    [TECH] Creation d'une classe utilitaire XkeyGamesUtils pour les 2 principales fonctions du xkey (list et launch) [TECH] Creation d'une classe d'exception applicative [TECH] Modification de GameDetailController et Xk3y4AndroidController, appel des fonctions de xKeyGamesUtils

--- Xk3y4AndroidLib/src/xk3y/dongle/android/Xk3y4AndroidController.java
+++ Xk3y4AndroidLib/src/xk3y/dongle/android/Xk3y4AndroidController.java
@@ -11,6 +11,8 @@ import org.acra.ErrorReporter;
 import xk3y.dongle.android.dto.FullGameInfo;
 import xk3y.dongle.android.dto.Iso;
 import xk3y.dongle.android.dto.Xkey;
+import xk3y.dongle.android.dto.XkeyResult;
+import xk3y.dongle.android.exception.XkeyException;
 import xk3y.dongle.android.ihm.SettingsActivity;
 import xk3y.dongle.android.ihm.theme.CoverFlowGamesActivity;
 import xk3y.dongle.android.ihm.theme.ListGamesActivity;
@@ -19,6 +21,7 @@ import xk3y.dongle.android.utils.DialogBoxUtils;
 import xk3y.dongle.android.utils.HttpServices;
 import xk3y.dongle.android.utils.LoadingUtils;
 import xk3y.dongle.android.utils.Xk3yParserUtils;
+import xk3y.dongle.android.utils.XkeyGamesUtils;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
@@ -89,7 +92,7 @@ public class Xk3y4AndroidController implements OnClickListener {
 	}
 
 	/**
-	 * Evenement r�lalis� lors d'un click sur un bouton
+	 * Evenement realise lors d'un click sur un bouton
 	 */
 	public void onClick(View v) {
 		SHOW_ERROR = false;
@@ -118,106 +121,27 @@ public class Xk3y4AndroidController implements OnClickListener {
 	 */
 	private void listGames() {
 		try {
-			SHOW_ERROR = false;
-			debugMsg = "List Games...";
-			String ipAdress = ConfigUtils.getConfig().getIpAdress();
-			if (ipAdress == null || (ipAdress != null && ipAdress.length() == 0)) {
+			
+			XkeyResult result =  XkeyGamesUtils.listGames();
+			sendMessage(UPDATE_FINISHED);
+			if (result.isShowError()){
 				SHOW_ERROR = true;
-				error_to_display = R.string.no_ip;
-				sendMessage(UPDATE_FINISHED);
-			}
-			urlToListGames = "http://" + ipAdress
-					+ "/data.xml";
-			String xkeyInfo = HttpServices.getInstance().getResponseFromUrl(
-					urlToListGames);
-			
-			ErrorReporter.getInstance().putCustomData("\n\nTHEME", String.valueOf(ConfigUtils.getConfig().getTheme()));
-			ErrorReporter.getInstance().putCustomData("\n\nDATA_XML", "\n" + xkeyInfo);
-			
-			debugMsg += "\nList Games OK";
-			if (xkeyInfo == null
-					|| (xkeyInfo != null && xkeyInfo.equals(""))) {
-				
-				debugMsg += "\nNo Xkey Connexion";
-				Xkey xkey = LoadingUtils.loadXkeyFromSdCard();
-				if (xkey == null) {
-					SHOW_ERROR = true;
-					error_to_display = R.string.not_connected;
+				error_to_display = result.getMessageCode();
+			}else{
+				Intent myIntent = null;
+				if (ConfigUtils.getConfig().addCoverTitle()) {
+					myIntent = new Intent(view, CoverFlowGamesActivity.class);
 				} else {
-					ConfigUtils.getConfig().setXkey(xkey);
+					myIntent = new Intent(view, ListGamesActivity.class);
 				}
-			} else {
-				// Generate xkey object from xml flow
-				debugMsg += "\nConvert XML to Object...";
-				Reader reader = new StringReader(xkeyInfo);
-				Xkey xkey = Xk3yParserUtils.getXkey(reader);
-				ConfigUtils.getConfig().setXkey(xkey);
-				debugMsg += "\nConvert XML to Object OK";
-			}	
-			
-			if (!SHOW_ERROR) {
-				List<Iso> listIsos = ConfigUtils.getConfig().getXkey().getListeGames();
-				if (listIsos == null || (listIsos != null && listIsos.isEmpty())) {
-					debugMsg += "\nNo Game detected";
-					SHOW_ERROR = true;
-					error_to_display = R.string.no_games;
-					
-					sendMessage(UPDATE_FINISHED);
-				} else {
-					Collections.sort(listIsos, Iso.TitleComparator);
-					ConfigUtils.getConfig().setListeAllGames(listIsos);
-					ConfigUtils.getConfig().setCurrentPage(1);
-					
-					/*
-					// Load game info
-					List<FullGameInfo> listGames = new ArrayList<FullGameInfo>();
-					int cptLoad = ConfigUtils.getConfig().getFirstGameToLoad();
-					int cptEnd = ConfigUtils.getConfig().getLastGameToLoad() + 1;
-					for (Iso game : ConfigUtils.getConfig().getListeIsoToLoad()){
-						cptLoad++;
-						currentGameNameToDebug = game.getTitle();
-						progressLoadingMessage = defaultLoadingMessage
-								+ "\nLoading game "+ cptLoad + "/" + cptEnd
-								+ "\n"+ game.getTitle();
-						sendMessage(UPDATE_PROGRESS);
-						listGames.add(LoadingUtils.loadGameInfo(game));
-	
-					}
-	
-					ConfigUtils.getConfig().setListeGames(listGames);
-					currentGameNameToDebug = "All Games load...";
-					 */
-					
-					sendMessage(UPDATE_FINISHED);
-					
-					// Open list games window
-					debugMsg += "\nLaunch CoverFlow...";
-					
-					Intent myIntent = null;
-					if (ConfigUtils.getConfig().addCoverTitle()) {
-						myIntent = new Intent(view, CoverFlowGamesActivity.class);
-					} else {
-						myIntent = new Intent(view, ListGamesActivity.class);
-					}
-					view.startActivity(myIntent);
-				}
+				view.startActivity(myIntent);
 			}
 			
-			
-		} catch (OutOfMemoryError E) {
-			//progressDialog.setMessage("boooom");
-			SHOW_ERROR = true;
-			error_to_display = R.string.out_of_memory_error;
-		} catch (Exception e) {
-			
-			ErrorReporter.getInstance().putCustomData("\n\nDEBUG_MESSAGE", "\n" + debugMsg);
+		
+		} catch (XkeyException e) {
+			ErrorReporter.getInstance().putCustomData("\n\nDEBUG_MESSAGE", "\n" + e.getMessage());
 			ErrorReporter.getInstance().handleException(e);
-
-			//progressDialog.setMessage("boooom");
-			//SHOW_DEBUG_MSG = true;
-			//debugMsg += "\n" + e.getMessage();
-			//error_to_display = R.string.error;
-		}
+		} 
 		
 	}
 
