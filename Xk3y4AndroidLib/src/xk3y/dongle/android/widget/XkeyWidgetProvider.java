1fb166321da054e5b8173981f5769e252c206836
gsourdrille <gsourdrille@gmail.com>
2013-05-16 13:29:17 +0000

    [WIDGET] premiers test

--- Xk3y4AndroidLib/src/xk3y/dongle/android/widget/XkeyWidgetProvider.java
+++ Xk3y4AndroidLib/src/xk3y/dongle/android/widget/XkeyWidgetProvider.java
@@ -2,6 +2,7 @@ package xk3y.dongle.android.widget;
 
 
 import xk3y.dongle.android.R;
+import xk3y.dongle.android.utils.XkeyGamesUtils;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -16,18 +17,39 @@ public class XkeyWidgetProvider extends AppWidgetProvider {
 			int[] appWidgetIds) {
 
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.xkey_widget_layout);
-		//remoteViews.setOnClickPendingIntent(R.id.widget_button, buildButtonPendingIntent(context));
-
+		remoteViews.setOnClickPendingIntent(R.id.prevButton, buildPrevButtonPendingIntent(context));
+		remoteViews.setOnClickPendingIntent(R.id.nextButton, buildNextButtonPendingIntent(context));
+		remoteViews.setOnClickPendingIntent(R.id.playButton, buildPlayButtonPendingIntent(context));
 		pushWidgetUpdate(context, remoteViews);
 	}
 
-	public static PendingIntent buildButtonPendingIntent(Context context) {
+	public static PendingIntent buildPrevButtonPendingIntent(Context context) {
 		Intent intent = new Intent();
-	    intent.setAction("pl.looksok.intent.action.CHANGE_PICTURE");
+	    intent.setAction("pl.looksok.intent.action.PREV_GAME");
+	    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	}
+	
+	public static PendingIntent buildNextButtonPendingIntent(Context context) {
+		Intent intent = new Intent();
+	    intent.setAction("pl.looksok.intent.action.NEXT_GAME");
+	    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	}
+	
+	public static PendingIntent buildPlayButtonPendingIntent(Context context) {
+		Intent intent = new Intent();
+	    intent.setAction("pl.looksok.intent.action.PLAY_GAME");
 	    return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 	}
 
 	public static void pushWidgetUpdate(Context context, RemoteViews remoteViews) {
+		
+		try {
+			XkeyGamesUtils.listGames();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		ComponentName myWidget = new ComponentName(context, XkeyWidgetProvider.class);
 	    AppWidgetManager manager = AppWidgetManager.getInstance(context);
 	    manager.updateAppWidget(myWidget, remoteViews);		
